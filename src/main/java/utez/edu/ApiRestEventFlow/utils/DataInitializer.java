3024930139c984784d0ffb803a545a015674da39
isaacOG145 <20233tn145@utez.edu.mx>
2025-03-08 11:21:41 -0600

    seguridad

---
+++ src/main/java/utez/edu/ApiRestEventFlow/utils/DataInitializer.java
@@ -0,0 +1,33 @@
+package utez.edu.ApiRestEventFlow.utils;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import utez.edu.ApiRestEventFlow.Role.Role; // Importa tu enum Role
+import utez.edu.ApiRestEventFlow.user.model.User;
+import utez.edu.ApiRestEventFlow.user.model.UserRepository;
+
+import java.util.Optional;
+
+@Configuration
+public class DataInitializer {
+
+    @Bean
+    CommandLineRunner initDatabase(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        return args -> {
+
+            // Crear un usuario SUPERADMIN si no existe
+            Optional<User> optionalSuperAdmin = userRepository.findByEmail("superadmin@example.com");
+            if (!optionalSuperAdmin.isPresent()) {
+                User superAdmin = new User();
+                superAdmin.setName("Super Admin");
+                superAdmin.setEmail("superadmin@example.com");
+                superAdmin.setPassword(passwordEncoder.encode("ultraSecret"));
+                superAdmin.setRole(Role.ADMIN);
+                userRepository.saveAndFlush(superAdmin);
+            }
+
+        };
+    }
+}
