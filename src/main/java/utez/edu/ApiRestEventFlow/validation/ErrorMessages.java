3aeda89ed8eb34cb7023e730f31d6165eebe7762
isaacOG145 <20233tn145@utez.edu.mx>
2025-03-08 10:11:14 -0600

    servicio usuarios checadores y admins

--- src/main/java/utez/edu/ApiRestEventFlow/validation/ErrorMessages.java
+++ src/main/java/utez/edu/ApiRestEventFlow/validation/ErrorMessages.java
@@ -10,7 +10,11 @@ public class ErrorMessages {
     public static final String EXCEPTION = "Ha ocurrido un error inesperado";
     public static final String INVALID_SENT_BY_USER = "El usuario que registra al checador debe ser un administrador";
     public static final String SENT_BY_USER_NOT_FOUND = "Administrador no encontrado";
+    public static final String SAME_PASSWORD = "La nueva contraseña no puede ser igual a la anterior";
+    public static final String INTERNAL_SERVER_ERROR = "Error interno del servidor";
     //sucessful
     public static final String SUCCESSFUL_REGISTRATION = "Regitro exitoso";
+    public static final String SUCCESFUL_UPDATE = "Actualizacion exitosa";
     public static final String SUCCESFUL_CHANGE_STATUS = "Se ha actualizado el estado a ";
+    public static final String SUCCESSFUL_PASSWORD_UPDATE = "Contraseña actualizada exitosamente";
 }
